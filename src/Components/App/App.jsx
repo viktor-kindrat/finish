db85e3d285acf471895eab24145ae3b10ffdd50e
Viktor Kindrat <kindratvictor07@gmail.com>
2023-07-19 10:56:17 +0300

    inital setup

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import './Styles/App.css';
+
+import Home from "../Home/Home"
+
+function App() {
+  return (
+    <div className="App">
+      APP
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Styles/Home.css"
+
+function Home (){
+    return (
+        <section className="Home">
+            <h2>Home page</h2>
+        </section>
+    )
+}
+
+export default Home
