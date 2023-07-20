2bad5189c37ac835bc881bfaaab7c72a9d64e907
Viktor Kindrat <kindratvictor07@gmail.com>
2023-07-20 19:21:48 +0300

    update. add responsive layout

--- src/Components/FlightCard/FlightCard.jsx
+++ src/Components/FlightCard/FlightCard.jsx
@@ -2,7 +2,7 @@ import "./Styles/FlightCard.css"
 
 import detailsIcon from "./SVG/details.svg"
 
-function FlightCard (){
+function FlightCard() {
     return (
         <article className="FlightCard">
             <div className="FlightCard__locations FlightCard__group">
@@ -20,11 +20,20 @@ function FlightCard (){
                 <p className="FlightCard__info">Доросилий</p>
                 <p className="FlightCard__info FlightCard__info_bigBold">5 048 <div className="FlightCard__info_bold">грн</div></p>
             </div>
-            <div className="FlightCard__group">
+            <div className="FlightCard__group FlightCard__group_uderlineonmobile">
                 <p className="FlightCard__info">Дитячий</p>
                 <p className="FlightCard__info FlightCard__info_bigBold">3 048 <div className="FlightCard__info_bold">грн</div></p>
             </div>
-            <button className="FlightCard__details-btn"><img src={detailsIcon} alt="details" /></button>
+            <div className="FlightCard__summary">
+                <div className="FlightCard__summary-raw">
+                    <div className="FlightCard__summary-sum">14 048 <sup>,00</sup></div>
+                    <div className="FlightCard__summary-currency">грн</div>
+                </div>
+                <div className="FlightCard__summary-info">
+                    Доросилий: 2, Дитячий: 1
+                </div>
+            </div>
+            <button className="FlightCard__book-btn"><p>Забронювати</p><img src={detailsIcon} alt="details" /></button>
         </article>
     )
 }
