de051193232b01f7741565ece0dda27a99f3eeb3
Viktor Kindrat <kindratvictor07@gmail.com>
2023-07-22 15:44:16 +0300

    update. ticket layout and some logic

--- src/Components/Ticket/Ticket.jsx
+++ src/Components/Ticket/Ticket.jsx
@@ -1,6 +1,6 @@
 import "./Styles/Ticket.css"
 
-function Ticket ({arrivalDate, canceled}){
+function Ticket({ arrivalDate, canceled }) {
     let getStatus = (arrivalDate) => {
         let now = new Date();
         let arrivalDateObject = new Date(arrivalDate);
@@ -11,9 +11,28 @@ function Ticket ({arrivalDate, canceled}){
         }
     }
     let status = canceled ? "canceled" : getStatus(arrivalDate)
+    let dateArrival = new Date(arrivalDate).toDateString().split(" ")
+    let timeArrival = new Date(arrivalDate).toTimeString().split(" ")[0].split(":")
     return (
         <article className={`Ticket Ticket_${status}`}>
-            TICKET with time {arrivalDate} status {status}
+            <div className="Ticket__locations">
+                <div className="Ticket__group">
+                    <div className="Ticket__info_bold Ticket__info">{`${timeArrival[0]}:${timeArrival[1]} ${dateArrival[0]}, ${dateArrival[2]} ${dateArrival[1]}`}</div>
+                    <div className="Ticket__info">Київ (Центральний автовокзал)</div>
+                </div>
+                <div className="Ticket__group">
+                    <div className="Ticket__sign">&#8594;</div>
+                </div>
+                <div className="Ticket__group">
+                    <div className="Ticket__info_bold Ticket__info">{`${timeArrival[0]}:${timeArrival[1]} ${dateArrival[0]}, ${dateArrival[2]} ${dateArrival[1]}`}</div>
+                    <div className="Ticket__info">Італія-Зюдкройц</div>
+                </div>
+            </div>
+            <div className="Ticket__group">
+                <div className="Ticket__info_bigBold">10 048 <sup className="Ticket__info_sup">,00</sup></div>
+                <div className="Ticket__info"> Дорослий: 1, Дитячий: 1</div>
+            </div>
+            <button disabled={status !== "active"} className="Ticket__action">{status === "canceled" ? "Скасовано" : status === "done" ? "Виконано" : status === "active" ? "Скасувати" : "Помилка"}</button>
         </article>
     )
 }
