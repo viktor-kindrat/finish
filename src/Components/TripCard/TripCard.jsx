3ccdc78f20e97b7767997104f8ace144276237cd
Viktor Kindrat <kindratvictor07@gmail.com>
2023-07-23 13:55:14 +0300

    update. layout styling. tripcard and tripcontrolls

--- src/Components/TripCard/TripCard.jsx
+++ src/Components/TripCard/TripCard.jsx
@@ -1,9 +1,30 @@
 import "./Styles/TripCard.css"
 
-function TripCard ({editOpened, setEditOpened, viewOpened, setViewOpened}) {
+function TripCard({ editOpened, setEditOpened, viewOpened, setViewOpened }) {
     return (
         <article className="TripCard">
-            TRIPCARD
+            <div className="TripCard__locations">
+                <div className="TripCard__group">
+                    <div className="TripCard__info TripCard__info_bold">20:45 пт, 3 лист.</div>
+                    <div className="TripCard__info">Україна - Київ (Центральний автовокзал)</div>
+                </div>
+                <div className="TripCard__sign">
+                    &#8594;
+                </div>
+                <div className="TripCard__group">
+                    <div className="TripCard__info TripCard__info_bold">сб, 4 лист.</div>
+                    <div className="TripCard__info">Італія - Санремо (Центральний автовокзал)</div>
+                </div>
+            </div>
+            <div className="TripCard__controll">
+                <h3 className="TripCard__headline">Україна - Італія</h3>
+                <div className="TripCard__places-info"><span className="TripCard__places-info_green">8</span>/<span className="TripCard__places-info_red">32</span></div>
+                <div className="TripCard__controll-btn-place">
+                    <button className="TripCard__controll-btn TripCard__controll-btn_green">Подробиці</button>
+                    <button className="TripCard__controll-btn TripCard__controll-btn_orange">Редагувати</button>
+                    <button className="TripCard__controll-btn TripCard__controll-btn_red">Видалити</button>
+                </div>
+            </div>
         </article>
     )
 }
