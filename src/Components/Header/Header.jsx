afa2e48629c0f89e5716bbafe672a8dffa6e5e30
Viktor Kindrat <kindratvictor07@gmail.com>
2023-08-17 17:59:04 +0300

    lazy loading. optimizations

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import "./Styles/Header.css"
 
-import logo from "./Images/logo.webp"
+import logo from "./SVG/logo.svg"
 import HeaderNavDesktop from "./Header-nav-desktop"
 import HeaderNavMobile from "./HeaderNavMobile/Header-nav-mobile"
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Styles/Home.css";
 import mainImage from "./Images/main.webp";
+import mainBetterImage from "./Images/mainBetterQual.webp";
 import HomeBooking from "./HomeBooking"
 import SearchingResult from "../SearchingResult/SearchingResult";
 
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useLocation } from "react-router-dom"
 
 const places = {
     "Україна": ["Київ", "Чернівці", "Коломия", "Франківськ", "Стрий", "Львів", "Мукачево"],
@@ -22,13 +23,29 @@ function Home({userData, setUserData, alertData, setAlertData, modalData, setMod
     let [triggerSearch, setTriggerSearch] = useState(false)
     let [passangerOpened, setPassangerOpened] = useState(false);
 
+    let location = useLocation()
+
+    useEffect(()=>{
+        let changeImageHandler = (e)=>{
+            if (document.querySelector(".Home__bgImage")){
+                document.querySelector(".Home__bgImage").src = mainBetterImage
+            }
+        }
+
+        document.querySelector(".Home__bgImage")?.addEventListener("load", changeImageHandler)
+
+        return ()=>{
+            document.querySelector(".Home__bgImage")?.removeEventListener("load", changeImageHandler)
+        }
+    }, [location])
+
     return (
         <section className="Home">
             <Routes>
                 <Route path="/" element={
                     <>
                         <div className="Home__head-group">
-                            <img className="Home__bgImage" src={mainImage} alt="main" />
+                            <img loading="lazy" className="Home__bgImage" src={mainImage} alt="main" />
                             <h1 className="Home__headline">Бронювання білетів</h1>
                         </div>
                         <HomeBooking {...{ triggerSearch, setTriggerSearch, searchingData, setSearchingData, date, setDate, places, peoples, setPeoples, from, setFrom, to, setTo, passangerOpened, setPassangerOpened }} />
